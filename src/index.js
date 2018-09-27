753d863f29594e3851cf3fa7c844eb22213c88d7
Cesar Avila Frias <cafavila@gmail.com>
2018-09-26 21:30:09 -0300

    Servidor GraphQl con prisma para hackernews

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// src/index.js
+
+const {GraphQLServer} = require('graphql-yoga')
+const {Prisma} = require('prisma-binding')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const AuthPayload = require('./resolvers/AuthPayload')
+const Subscription = require('./resolvers/Subscription')
+const Feed = require('./resolvers/Feed')
+
+const resolvers = {Query, Mutation, AuthPayload, Subscription, Feed}
+const server = new GraphQLServer({typeDefs: './src/schema.graphql', resolvers, context: req =>
+  ({
+    ...req, db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'http://localhost:4466/hydrogendb/dev',
+      secret: 'hydroadmin123',
+      debug: true
+    })
+  })})
+
+server.start(()=>{console.log('[HYDROGEN-SERVER] - Servidor en estado RUNNING')})
